1a362d0af4003ab790aef628ad6600f5338e889f
hann <wjdgks1233@gmail.com>
2025-04-10 22:03:35 +0900

    feat : restaurant entity #1

---
+++ src/main/java/com/example/umc8th/domain/restaurant/Restaurant.java
@@ -0,0 +1,30 @@
+package com.example.umc8th.domain.restaurant;
+
+import com.example.umc8th.domain.foodType.FoodType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_id")
+    private Long id;
+
+    private String name;
+
+    private String imgUrl;
+
+    private String address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_type_id")
+    private FoodType foodType;
+
+
+}
