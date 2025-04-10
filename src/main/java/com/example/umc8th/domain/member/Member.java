c317bee7e93b868bde1303116011f23f6c9f3605
hann <wjdgks1233@gmail.com>
2025-04-10 21:56:26 +0900

    feat : member entity & relations #1

---
+++ src/main/java/com/example/umc8th/domain/member/Member.java
@@ -0,0 +1,42 @@
+package com.example.umc8th.domain.member;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String nickname;
+
+    private String address;
+
+    private String specAddress;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+}
---
+++ src/main/java/com/example/umc8th/domain/member/SocialType.java
@@ -0,0 +1,5 @@
+package com.example.umc8th.domain.member;
+
+public enum SocialType {
+    KAKAO, GOOGLE, NAVER, APPLE
+}
