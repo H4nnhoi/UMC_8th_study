827d4ff77a52b1efaad449ad599914ff48e367f9
hann <wjdgks1233@gmail.com>
2025-05-03 21:55:56 +0900

    refactor : add extends interface in memberRepository #3

--- src/main/java/com/example/umc8th/repository/member/MemberRepository.java
+++ src/main/java/com/example/umc8th/repository/member/MemberRepository.java
@@ -3,5 +3,5 @@ package com.example.umc8th.repository.member;
 import com.example.umc8th.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 }
