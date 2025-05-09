c8bdfa3d0968cda57b740de5b6fc0f5aa316b844
hann <wjdgks1233@gmail.com>
2025-05-09 19:50:59 +0900

    feat : temp dto & controller #5

---
+++ src/main/java/com/example/umc8th/converter/TempConverter.java
@@ -0,0 +1,12 @@
+package com.example.umc8th.converter;
+
+import com.example.umc8th.web.dto.temp.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO(){
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is Test!")
+                .build();
+    }
+}
